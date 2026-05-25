4d41028e06e6ce524970bd4ee0b08a80f63f1a34
SkodriNON <skodrinon.official@gmail.com>
2026-05-25 12:55:55 +0100

    final history fix

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,22 +1,9 @@
 
 export default function HistoryPage() {
-
   return (
-
-    <main className="
-      min-h-screen
-      flex
-      items-center
-      justify-center
-      bg-black
-      text-white
-    ">
-
+    <div>
       History Page
-
-    </main>
+    </div>
   )
 }
-
-
 
