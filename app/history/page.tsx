6d46618a5f3c69ca49a3d9358f820878ed8e3f75
SkodriNON <skodrinon.official@gmail.com>
2026-05-25 12:49:43 +0100

    restore history page

---
+++ app/history/page.tsx
@@ -0,0 +1,20 @@
+
+export default function HistoryPage() {
+
+  return (
+
+    <main className="
+      min-h-screen
+      flex
+      items-center
+      justify-center
+      bg-black
+      text-white
+    ">
+
+      History Page
+
+    </main>
+  )
+}
+
